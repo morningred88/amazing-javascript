d4c38ff91bfbd2738b0501d71f867622a90f5b04
Lily Ma <xma50089@gmail.com>
2021-09-09 22:37:18 -0500

    Example of JS dynamic typing added

---
+++ dynamic-typing.js
@@ -0,0 +1,13 @@
+//JS does not require the explicit declaration of the variables before they are used. 
+//The value of a variable decide the data type 
+let age;
+console.log(typeof age)         //undefined
+
+age = 35;
+console.log(typeof age)        //number
+
+age = 'young';
+console.log(typeof age)        //string
+
+age = true
+console.log(typeof age)         //boolean
