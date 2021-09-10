a3487bb1259e63c33d8736d6e18671add2852947
Lily Ma <xma50089@gmail.com>
2021-09-09 22:26:35 -0500

    typeof operator added

---
+++ typeof-operator.js
@@ -0,0 +1,7 @@
+//typeof is not a function. Like plus, minus, is an operator
+console.log(typeof true);       //boolean
+console.log(typeof false);      //boolean
+console.log(typeof 3);          //number
+console.log(typeof 5.5);        //number
+console.log(typeof 'Tom');      //string
+
